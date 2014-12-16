e9fda1de0767919141d931374a2fe8efeabb6e2b
nathanXplosion <12030121044@sicsr.ac.in>
2014-12-16 09:45:42 +0530

    OnClick

--- src/com/example/eventhandling1/MainActivity.java
+++ src/com/example/eventhandling1/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.eventhandling1;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	private Button click;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		click=(Button) findViewById(R.id.mvb);
+		
+		click.setText("Go on.");
+		
+		click.setTextColor(Color.GREEN);
+		click.setBackgroundColor(Color.YELLOW);
+		
+		click.setTextSize((float)12.3);
+		
+		OnClickListener listener=new OnClickListener(){
+			
+			@Override
+			public void onClick(View view){
+				click.setText("Button was clicked");
+			}
+			
+		};
 	}
 
 	@Override
